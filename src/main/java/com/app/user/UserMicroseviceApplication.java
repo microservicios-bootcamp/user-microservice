f56ee436c01223254695d4efadf5424740440a84
Moises Molina <w7moises@gmail.com>
2022-04-06 10:29:19 -0500

    user template added

---
+++ src/main/java/com/app/user/UserMicroseviceApplication.java
@@ -0,0 +1,13 @@
+package com.app.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMicroseviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserMicroseviceApplication.class, args);
+    }
+
+}
