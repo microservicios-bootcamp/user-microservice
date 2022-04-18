9d13424ecfa4a55f525f2be70cf35480a6025eaf
Moises Molina <w7moises@gmail.com>
2022-04-18 11:06:23 -0500

    change private method for controllers

--- src/main/java/com/demo/app/user/controllers/EnterpriseController.java
+++ src/main/java/com/demo/app/user/controllers/EnterpriseController.java
@@ -1,9 +1,7 @@
 package com.demo.app.user.controllers;
 
 import com.demo.app.user.entities.Enterprise;
-import com.demo.app.user.entities.Personal;
 import com.demo.app.user.models.CurrentAccountType;
-import com.demo.app.user.models.SavingAccountType;
 import com.demo.app.user.services.EnterpriseService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,34 +18,34 @@ public class EnterpriseController {
     }
 
     @GetMapping
-    private ResponseEntity<Flux<Enterprise>> findAll(){
+    public ResponseEntity<Flux<Enterprise>> findAll(){
         Flux<Enterprise> enterprise = enterpriseService.findAll();
         return ResponseEntity.ok(enterprise);
     }
 
     @GetMapping("/{id}")
-    private Mono<ResponseEntity<Enterprise>> findById(@PathVariable String id){
+    public Mono<ResponseEntity<Enterprise>> findById(@PathVariable String id){
         return enterpriseService.findById(id).map(ResponseEntity::ok).defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
     @PostMapping("saveCurrentAccount/{type}")
-    private ResponseEntity<Mono<Enterprise>> saveCurrentAccount(@RequestBody Enterprise enterprise,@PathVariable CurrentAccountType type){
+    public ResponseEntity<Mono<Enterprise>> saveCurrentAccount(@RequestBody Enterprise enterprise,@PathVariable CurrentAccountType type){
         Mono<Enterprise> result;
         if (type.equals(CurrentAccountType.NORMAL)) result = enterpriseService.saveNormalCurrentAccount(enterprise);
         else result = enterpriseService.savePymeCurrentAccount(enterprise);
         return ResponseEntity.ok(result);
     }
     @PostMapping("saveCreditAccount")
-    private ResponseEntity<Mono<Enterprise>> saveCreditAccount(@RequestBody Enterprise enterprise){
+    public ResponseEntity<Mono<Enterprise>> saveCreditAccount(@RequestBody Enterprise enterprise){
         return ResponseEntity.ok(enterpriseService.saveCreditAccount(enterprise));
     }
     @PutMapping("/{id}")
-    private Mono<ResponseEntity<Enterprise>> update(@RequestBody Enterprise enterprise, @PathVariable String id){
+    public Mono<ResponseEntity<Enterprise>> update(@RequestBody Enterprise enterprise, @PathVariable String id){
         return enterpriseService.update(enterprise,id).map(ResponseEntity::ok).defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{id}")
-    private Mono<ResponseEntity<String>> delete(@PathVariable String id){
+    public Mono<ResponseEntity<String>> delete(@PathVariable String id){
         return enterpriseService.delete(id).map(x->ResponseEntity.ok("Id eliminado"+id)).defaultIfEmpty(ResponseEntity.notFound().build());
     }
 }
--- src/main/java/com/demo/app/user/controllers/PersonalController.java
+++ src/main/java/com/demo/app/user/controllers/PersonalController.java
@@ -18,37 +18,37 @@ public class PersonalController {
     }
 
     @GetMapping
-    private ResponseEntity<Flux<Personal>> findAll(){
+    public ResponseEntity<Flux<Personal>> findAll(){
         Flux<Personal> personal = personalService.findAll();
         return ResponseEntity.ok(personal);
     }
     @GetMapping("/{id}")
-    private Mono<ResponseEntity<Personal>> findById(@PathVariable String id){
+    public Mono<ResponseEntity<Personal>> findById(@PathVariable String id){
         return personalService.findById(id).map(ResponseEntity::ok).defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
     @PostMapping("/saveCurrentAccount")
-    private ResponseEntity<Mono<Personal>> saveCurrentAccount(@RequestBody Personal personal){
+    public ResponseEntity<Mono<Personal>> saveCurrentAccount(@RequestBody Personal personal){
         return ResponseEntity.ok(personalService.saveCurrentAccount(personal));
     }
     @PostMapping("/saveSavingAccount/{type}")
-    private ResponseEntity<Mono<Personal>> saveSavingAccount(@RequestBody Personal personal, @PathVariable SavingAccountType type){
+    public ResponseEntity<Mono<Personal>> saveSavingAccount(@RequestBody Personal personal, @PathVariable SavingAccountType type){
         Mono<Personal> result;
         if (type.equals(SavingAccountType.NORMAL)) result = personalService.saveNormalSavingAccount(personal);
         else result = personalService.saveVipSavingAccount(personal);
         return ResponseEntity.ok(result);
     }
     @PostMapping("/saveFixedTermAccount")
-    private ResponseEntity<Mono<Personal>> saveFixedTermAccount(@RequestBody Personal personal){
+    public ResponseEntity<Mono<Personal>> saveFixedTermAccount(@RequestBody Personal personal){
         return ResponseEntity.ok(personalService.saveFixedTermAccount(personal));
     }
     @PutMapping("/{id}")
-    private Mono<ResponseEntity<Personal>> update(@RequestBody Personal personal, @PathVariable String id){
+    public Mono<ResponseEntity<Personal>> update(@RequestBody Personal personal, @PathVariable String id){
         return personalService.update(personal,id).map(ResponseEntity::ok).defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{id}")
-    private Mono<ResponseEntity<Void>> delete(@PathVariable String id){
+    public Mono<ResponseEntity<Void>> delete(@PathVariable String id){
         return personalService.delete(id).map(ResponseEntity::ok).defaultIfEmpty(ResponseEntity.notFound().build());
     }
 }
